c582f925dafc3ef311c8cf10f5ceb453b5bcafd7
i549 <31362959+i549@users.noreply.github.com>
2017-11-13 14:04:29 +0800

    Create LoadResource.java

---
+++ node-spring/LoadResource.java
@@ -0,0 +1,10 @@
+public class LoadResource {
+    public static void test(){
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        String suffix = ExportConstants.RES_FONT + File.separator + name;
+        Resource[] resource = resolver.getResources("classpath*:" + suffix);
+        if (null != resource && resource.length > 0) {
+            System.out.println(resource[0].getURL());
+        }
+    }
+}
