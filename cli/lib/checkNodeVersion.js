7091230949ed05ab907f97280963be8bc8e5c69a
i549 <31362959+i549@users.noreply.github.com>
2020-02-28 16:04:31 +0800

    Create checkNodeVersion.js

---
+++ cli/lib/checkNodeVersion.js
@@ -0,0 +1,18 @@
+/**
+ * node version check
+ */
+module.exports = () => {
+    const version = process['versions']['node'];
+    const semver = version.split('.');
+    const major = semver[0];
+
+    if (major < 8) {
+        console.error(
+            'You are running Node ' +
+            version + '.\n' +
+            '@pinnet/cli requires Node 8 or higher.\n' +
+            'Please update your version of Node.'
+        );
+        process.exit(1);
+    }
+};
